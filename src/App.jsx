33a22b934273217ca801e90c09eb6df6f015db1f
Prakash-7382012092 <prakashprakkey@gamil.com>
2025-02-15 13:45:57 +0530

    zxc

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import { createHashRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Cart from './components/Cart';
 import RootLayout from './components/RootLayout';
 
 const App = () => {
-  const router = createBrowserRouter(
+  const router = createHashRouter(  // Changed from createBrowserRouter to createHashRouter
     createRoutesFromElements(
       <Route path='/' element={<RootLayout />}>
         <Route index element={<Home />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,9 @@ const Navbar = () => {
     };
 
     return (
-        <div className='bg-green-500 h-[100px]  text-white max-w-[100%] mx-auto flex justify-between items-center px-4'>
+        <div className='bg-green-500 h-[100px] text-white max-w-[100%] mx-auto flex justify-between items-center px-4 
+                        fixed top-0 left-0 w-full z-50 shadow-md'>
+
             {/* Logo */}
             <h1 className='text-3xl font-bold primary-color'>Prakash</h1>
 
@@ -34,28 +36,28 @@ const Navbar = () => {
             </ul>
 
             {/* Mobile Menu Button */}
-            {nav ? <div></div>: <div className='md:hidden sm:block'><Link to="/cart" className='no-underline link'>
-    <span className="text-green-200 top-2 right-1 text-3xl flex">
-        <CgShoppingCart className="text-white" /> &nbsp;
-        <b className="text-white no-underline">{car}</b> {/* Applied fix here */}
-    </span>
-</Link></div>}
+            {nav ? <div></div> : (
+                <div className='md:hidden sm:block'>
+                    <Link to="/cart" className='no-underline link'>
+                        <span className="text-green-200 top-2 right-1 text-3xl flex">
+                            <CgShoppingCart className="text-white" /> &nbsp;
+                            <b className="text-white no-underline">{car}</b>
+                        </span>
+                    </Link>
+                </div>
+            )}
 
             <div onClick={handleNav} className='block md:hidden cursor-pointer'>
-                {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} /> }
+                {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
             </div>
-
-         
 
             {/* Mobile Navigation Sidebar */}
             <div className={nav ? 'fixed h-full left-0 top-0 w-full bg-green-900 ease-in-out duration-500 z-50' : 'fixed left-[-100%]'}>
                 <h1 className='text-3xl text-green-500 m-4'>Prakash</h1>
                 <AiOutlineClose className="absolute top-2 right-1 text-green-600 text-3xl cursor-pointer" onClick={handleNav} />
-               
 
                 <ul className='p-8 text-2xl'>
-                    <li className='p-2'><Link to="/" >Home</Link></li>
-                 
+                    <li className='p-2'><Link to="/">Home</Link></li>
                     <li className='p-2'><Link to="/cart">Cart</Link></li>
                 </ul>
             </div>
